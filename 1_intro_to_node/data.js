31926ecfc558692f2960315dad1e790ffc8c65f6
kristinmunn <59896056+kristinmunn@users.noreply.github.com>
2020-04-20 16:11:18 -0700

    Add files via upload
    Uploaded 1_into_to_node files

---
+++ 1_intro_to_node/data.js
@@ -0,0 +1,12 @@
+let albums = [
+    {title: 'Nothing Shocking', artist: Janes Addiction, studio: Eldorado Studios, genre: alternative rock },
+    {title: 'Tea For The Tillerman', artist: Cat Stevens, studio: Morgan Studios, Island Studios, Olympic Studios, genre: folk rock },
+    {title: 'High as Hope', artist: Florence + the Machine, studio: Republic Records and Virgin EMI Records, genre: alternative/Indie },
+    {title: 'Come Away With Me', artist: Norah Jones, studio: Sorcerer Sound Studio, genre: folk },
+    {title: 'Purple Rain', artist: Prince and The Revolution, studio: First Avenue, genre: pop, rock },
+];
+
+exports.albums = albums
+
+console.log("length " + items.length)
+
